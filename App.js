8278e9ab531b7a21edff19ec200d5ea13b61f6dd
Arecio Canton <cuba289@gmail.com>
2021-06-28 22:42:19 -0400

    add task functionality complete

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Tasks from './components/Task'
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
   return (
     <View style={styles.container}>
 
@@ -12,8 +21,11 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go */}
-          <Tasks text={'Task 1'} />
-          <Tasks text={'Task 2'} />
+          {
+            taskItems.map((item, index) => {
+              return <Tasks key={index} text={item} />
+            })
+          }
         </View>
       </View>
 
@@ -22,9 +34,9 @@ export default function App() {
       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.writeTaskWrapper}>
 
-        <TextInput style={styles.input} placeholder={'Write your tasks'} />
+        <TextInput returnKeyType='done' style={styles.input} placeholder={'Write your tasks'} value={task} onChangeText={text => setTask(text)}/>
 
-        <TouchableOpacity >
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
@@ -77,6 +89,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderColor: '#C0C0C0',
     borderWidth: 1,
+    margin: 5
   },
   addText: {},
 });
